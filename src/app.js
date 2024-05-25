e0d790e4aaebb39ba1bc9ef5b6acec22f9212e3c
Mohammad Aqeel <123291892+MOHAMMADAQEEL8859@users.noreply.github.com>
2024-05-25 16:47:34 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-// Import required modules
+// These modules are required for this project
 const express = require('express');
 const Subscriber = require('./models/subscribers');
 const path = require('path');
@@ -6,20 +6,22 @@ const path = require('path');
 
 const app = express();
 
-// Middleware setup
+/* Here we are setting up the middleware. A middleware can be used for many purpose, like we have to execute some code before sending the main response,
+ or we can use middleware in authorization. We create middleware using app.use() method.*/
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
-// Serve static files from the 'public' directory
+
+// Here we are using this middleware to serve static files from the 'public' directory
 app.use(express.static('public'));
 
-// Route to serve the index.html file
+// This get Route to serve the index.html file means it will open the main page of this application.
 app.get('/', (req, res)=>{
     res.status(200).sendFile(path.join(__dirname, "/index.html"));
 });
 
 
-// Route to get all subscribers
+// This is the subscriber endpoint. When a request come to this endpoint then this endpoint provide you the all information of subscribers.
 app.get('/subscribers',async(req,res)=>{
     try{
         let subscribers = await Subscriber.find();               // Fetch all subscribers
@@ -30,12 +32,13 @@ app.get('/subscribers',async(req,res)=>{
     }    
 });
 
-// Route to get only names and subscribed channels of subscribers
+// When a get request with this Route (/subscriber/names), It will provide you names and subscribed channels of subscribers
+
 app.get('/subscribers/names',async(req,res)=>{
     try{
         let subscribers = await Subscriber.find(
-            {}, 
-            { name: 1, subscribedChannel: 1, _id: 0 }           // Fetch subscribers with only name and subscribedChannel fields
+            {},                                                 
+            { name: 1, subscribedChannel: 1, _id: 0 }   // Here you can se 1 and 0=>  1 means field we want to inlude  and 0 for field we want to exclude in result.
         );
         res.status(200).send(subscribers); 
     }
@@ -44,14 +47,16 @@ app.get('/subscribers/names',async(req,res)=>{
     }
 });
 
-// Route to get a subscriber by ID
+/* This endpoint helped you to fetch data of a Subscriber. You can find a subscriber by putting his/her id in input. If id not macth in databse it will show you
+No Subscriber found in  this id. */
+
 app.get('/subscribers/:id',async (req, res)=>{
-    try{                                                                   //fetch data of a subscriber with subscriber ID
+    try{                                                                   
         let subscribers = await Subscriber.findById(req.params.id);
         // Response data
         res.status(200).send(subscribers);
     }
-    catch(err) {                               // Handle invalid ID error
+    catch(err) {                          
         res.status(400).send({Error_message : "No Subscriber found in  this id"});
     }
 });
